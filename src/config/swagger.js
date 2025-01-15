12df468f06ab24ab1b9b654a6fb9d1ec5cbcff4f
Eduardo Pires Carvalho <carva.ep@gmail.com>
2025-01-15 14:33:25 -0300

    feat: config swagger

---
+++ src/config/swagger.js
@@ -0,0 +1,29 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "Descrição da sua API",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000 ",
+        description: "Servidor local",
+      },
+    ],
+  },
+  apis: ["../routes/*.js"], // local do arquivo da rotas
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+const setupSwagger = (app)=>{
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = setupSwagger;
